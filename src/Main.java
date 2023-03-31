658f285343b468524d4a8884c1a886629b27b18a
Nataliamg13 <nataliamg13@gmail.com>
2023-03-30 22:11:04 -0500

    The lights have been added

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,12 @@ public class Main {
       X.cylinders="8";
       X.size="123cc";
       X.weight="6453k";
-      
 
-      
+    lights neon =new lights();
+    neon.color="red";
+    neon.size="15cm";
+    neon.intensity="7675lumin";
+
+
     }
 }
